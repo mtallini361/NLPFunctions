e1c4d04ab9f831ae859ae1b20bf99bee5321f879
Michael Tallini <michael.tallini@90north.health>
2024-01-12 21:52:42 -0500

    added fixture strings

---
+++ src/functions/tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def string1():
+    return "execution"
+
+
+@pytest.fixture
+def string2():
+    return "intention"
--- src/functions/tests/test_similarity.py
+++ src/functions/tests/test_similarity.py
@@ -1,4 +1,5 @@
 from functions.similarity import minimal_edit_distance
 
+
 def test_minimal_edit_distance(string1, string2):
-    pass
+    minimal_edit_distance(string1, string2)
